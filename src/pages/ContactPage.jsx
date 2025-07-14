7a74bc01fb5e1366cc59b7f0022cf35b925ff569
Christopher <christopher.ferraro34@gmail.com>
2025-07-13 20:30:43 -0400

    contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,20 +1,39 @@
+import React from 'react';
+import '../styles/ContactPage.css';
+import Container from '../components/Container';
+import BlobGradient from '../components/BlobGradient';
+import rdsLogo from '../assets/RDS_Nav.svg';
+
 export default function ContactPage() {
     return (
-        <div className="contact-page">
-        <h1>Contact Us</h1>
-        <p>If you have any questions or would like to work with us, please fill out the form below:</p>
-        <form>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required />
-    
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-    
-            <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" required></textarea>
-    
-            <button type="submit">Send</button>
-        </form>
-        </div>
+        <Container>
+            <div className="contact-page">
+                <div className="contact-image">
+                    <img src={rdsLogo} alt="Rado Design Studio Logo" className="logo" />
+                    <BlobGradient />
+                </div>
+                <h2 className="title">Put Ideas in Motion</h2>
+                
+                <form>
+                    <input type="text" id="name" name="name" placeholder="Name (Business or Personal)" required />
+            
+                    <input type="email" id="email" name="email" placeholder="Email" required />
+
+                    <select id="project-type" name="project-type" required>
+                        <option value="" disabled selected>Project Type</option>
+                        <option value="web">Web</option>
+                        <option value="brand">Brand</option>
+                        <option value="web-brand">Web + Brand</option>
+                        <option value="promo">Promo</option>
+                    </select>
+
+                    <input type="text" id="phone" name="phone" placeholder="Phone (optional)" />
+            
+                    <textarea id="message" name="message" placeholder="Message" required></textarea>
+            
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </Container>
     );
     }
